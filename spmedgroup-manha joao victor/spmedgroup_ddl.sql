bd5c611241e445867b33d1525513bb91071d3c8f
jauteixeira <joao_peko@outlook.com>
2020-02-11 10:10:45 -0300

    projeto e tals

---
+++ spmedgroup-manha joao victor/spmedgroup_ddl.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE spMedGroupManha
+
+USE spMedGroupManha
+
+--DDL
+
+CREATE TABLE tipoUsuario
+	(
+		idTipoUsuario		INT PRIMARY KEY IDENTITY,
+		tituloTipoUsuario	VARCHAR(255),
+	);
+
+CREATE TABLE especialidade
+	(
+		idEspecialidade     INT PRIMARY KEY IDENTITY,
+		nomeEspecialidade	VARCHAR(255),
+	);
+
+CREATE TABLE clinica
+	(
+		idClinica			INT PRIMARY KEY IDENTITY,
+		nomeFantasia		VARCHAR(255),
+		CNPJ				CHAR(14),
+		endereco			VARCHAR(255),
+		telefone			VARCHAR(15),
+		horarioFunc			TIME
+	);
+
+CREATE TABLE medico
+	(
+		idMedico			INT PRIMARY KEY IDENTITY,
+		email				VARCHAR(255),
+		crm					VARCHAR(100),
+		senha				VARCHAR(255),
+		idTipoUsuario		INT FOREIGN KEY REFERENCES tipoUsuario (idTipoUsuario),
+		idClinica			INT FOREIGN KEY REFERENCES clinica (idClinica),
+		idEspecialidade		INT FOREIGN KEY REFERENCES especialidade(idEspecialidade)
+	);
+
+CREATE TABLE usuario
+	(
+		idUsuario			INT PRIMARY KEY IDENTITY,
+		nomeUsuario			VARCHAR(255),
+		email				VARCHAR(255),
+		RG					VARCHAR(255),
+		CPF					VARCHAR(255),
+		senha				VARCHAR(255),
+		endereco			VARCHAR(255),
+		dataNasc			DATE,
+		idTipoUsuario		INT FOREIGN KEY REFERENCES tipoUsuario (idTipoUsuario)
+	);
+
+CREATE TABLE consulta
+	(
+		idConsulta			INT PRIMARY KEY IDENTITY,
+		descricao			VARCHAR(255),
+		situacao			VARCHAR(255),
+		dataConsulta		DATETIME
+	);
+	
